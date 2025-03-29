818fabb58cede1a7dc07de8472af48c1f16b4302
LukaUdovc <luka.udovc@student.um.si>
2025-03-30 00:05:50 +0100

    Funkcija manjsaj_sliko in implementacija v main

--- naloga1.py
+++ naloga1.py
@@ -2,8 +2,9 @@ import cv2 as cv
 import numpy as np
 import time
 
+
 def zmanjsaj_sliko(slika, sirina, visina):
-    pass
+    return cv.resize(slika,(sirina,visina))
 
 def obdelaj_sliko_s_skatlami(slika, sirina_skatle, visina_skatle, barva_koze) -> list: #preveri kje so polja barve koze
     visina, sirina, _ = slika.shape  # pridobimo višino in širino slike
@@ -18,15 +19,20 @@ def obdelaj_sliko_s_skatlami(slika, sirina_skatle, visina_skatle, barva_koze) ->
         rezultat.append(vrstica) #celo vrstico daj v rezultat
     return rezultat
 
-
 def prestej_piklse_z_barvo_koze(slika, barva_koze) -> int:
     spodnja, zgornja = barva_koze
     maska = cv.inRange(slika, spodnja, zgornja) #ustvari crno belo masko ki vraca kje na sliki je so piksli koze(0,255)
     return cv.countNonZero(maska) #vrne stevilo pikslov kozne barve v skatli
 
-
 def doloci_barvo_koze(slika,levo_zgoraj,desno_spodaj) -> tuple: #določi barvo kože glede na povprečno barvo v kvadratu
-    pass
+    x1, y1 = levo_zgoraj 
+    x2, y2 = desno_spodaj
+    podslika = slika[y1:y2, x1:x2]#izrezek slike kjer naj bi bila koza
+    povprecje = np.mean(podslika.reshape(-1, 3), axis=0)#izracuna povprecje barve koze v kvadratu, vsaka vrstica stevilo pikslov x 3 barvne vrednosti
+    toleranca = 40  # dodatek da pokrije tudi malo svetlejse in temnejse
+    spodnja = np.clip(povprecje - toleranca, 0, 255).astype(np.uint8) #clip uporabljen da vrednosti manjse od min postanejo min/vecje od max postanejo max
+    zgornja = np.clip(povprecje + toleranca, 0, 255).astype(np.uint8)
+    return (spodnja, zgornja) #vrne spodno pa zgorno mejo barve
 
 if __name__ == '__main__':
     kamera = cv.VideoCapture(0)
@@ -42,16 +48,16 @@ if __name__ == '__main__':
         ret, slika = kamera.read() #zajemi sliko iz kamere
         if not ret:
             continue
-    
 
+        slika = zmanjsaj_sliko(slika, SIRINA, VISINA) #zmanjsaj sliko
         cv.rectangle(slika, (100, 100), (160, 160), (0, 255, 0), 2) #narisi kvadrat
         cv.putText(slika, "Pritisni 's' za zajem barve koze", (10, 20), #navodila
-                cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+                   cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
         cv.imshow("Zajem barve koze", slika) #prikazi sliko z kvadratom
 
         tipka = cv.waitKey(1) #pocakaj na pritisk gumba
         if tipka == ord('s'):
-
+            barva_koze = doloci_barvo_koze(slika, (100, 100), (160, 160)) #določi barvo kože z zajemom barve v kvadratu
             break
         elif tipka == 27:  # ESC
             kamera.release()
@@ -59,7 +65,7 @@ if __name__ == '__main__':
             exit()
 
     cv.destroyWindow("Zajem barve koze")
-    
+
     # Glavna zanka
     while True:
         start = time.time()
